b741c73777d1743c2ffde10c0c52d528a6263ce3
ERROR 13660 <error13660@gmail.com>
2026-07-24 14:20:26 +0200

    Vein generation
    untested

--- Assets/Scripts/TileManager.cs
+++ Assets/Scripts/TileManager.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class TileManager : MonoBehaviour
@@ -14,6 +15,18 @@ public class TileManager : MonoBehaviour
     private HashSet<Vector2Int> minedTiles;
 
     [SerializeField] public int spawnDistance;
+
+    private static Vector2Int[] OFFSETS =
+        {
+            new Vector2Int(-1,-1),
+            new Vector2Int(0,-1),
+            new Vector2Int(1,-1),
+            new Vector2Int(-1,0),
+            new Vector2Int(1,0),
+            new Vector2Int(-1,1),
+            new Vector2Int(0,1),
+            new Vector2Int(1,1),
+        };
 
     private void Awake()
     {
@@ -35,7 +48,7 @@ public class TileManager : MonoBehaviour
             for (int j = 0; j < mapHeight; j++)
             {
                 Vector2Int pos = new Vector2Int(i, j);
-                float height = pos.y + (int)(NoisePatterns.Instance.Pattern1(pos*2) * 30f);
+                float height = pos.y + (int)(NoisePatterns.Instance.Pattern1(pos * 2) * 30f);
 
                 //tid 0
                 if (height < 30) SetTileId(pos, 0);
@@ -43,12 +56,14 @@ public class TileManager : MonoBehaviour
             }
         }
 
-
         //place veins
         /*
          * On top of the base materials of biomes, biome specific vein-like clumps of special materials can appear.
          * Thes can be siginificantly harder or easier to mine, making optimal traversal more exciting.
          */
+
+        //place clay veins
+
     }
 
     void Start()
@@ -132,5 +147,111 @@ public class TileManager : MonoBehaviour
     private bool IsTileMined(Vector2Int pos)
     {
         return minedTiles.Contains(pos);
+    }
+
+    private Vector2Int[] GetVeinBasePoints(int numberOfPoints)
+    {
+        var points = new Vector2Int[numberOfPoints];
+        for (int i = 0; i < numberOfPoints; i++)
+        {
+            points[i] = new Vector2Int(
+                ((mapWith / numberOfPoints) / 2) + ((mapWith / numberOfPoints) * i),
+                0);
+        }
+        return points;
+    }
+    private Vector2Int[] GetNextVeinSpawnPoints(Vector2Int[] prev, Func<Vector2Int, Vector2Int> offsetFunc)
+    {
+        Vector2Int[] next = new Vector2Int[prev.Length];
+        for (int i = 0; i < prev.Length; i++)
+        {
+            next[i] = offsetFunc.Invoke(prev[i]);
+        }
+        return next;
+    }
+
+    /// <summary>
+    /// Generates a vein
+    /// </summary>
+    /// <param name="spawnPoint">The starting point of the vein generation</param>
+    /// <param name="iterations">How many tiles to place</param>
+    /// <param name="valueFunc">
+    /// Vector2: direction from the spawn point
+    /// int: number of neighboring vein tiles
+    /// float: distance from spawn point
+    /// returns: value (higher is better)
+    /// </param>
+    private void GenerateVein(Vector2Int spawnPoint, int iterations, int tileId, Func<Vector2, int, float, float> valueFunc)
+    {
+        List<Vector2Int> workingTiles = new List<Vector2Int>();
+        void AddValidTiles(Vector2Int[] tiles)
+        {
+            for (int i = 0; i < tiles.Length; i++)
+            {
+                if (GetTileId(tiles[i]) == tileId) continue;
+                if (workingTiles.Contains(tiles[i])) continue;
+                workingTiles.Add(tiles[i]);
+            }
+        }
+
+        SetTileId(spawnPoint, tileId);
+        AddValidTiles(NeighboringTiles(spawnPoint));
+
+        for (int i = 0; i < iterations; i++)
+        {
+            Vector2Int bestPos = workingTiles[0];
+            float bestValue = valueFunc.Invoke(
+                (Vector2)(workingTiles[0] - spawnPoint),
+                NumberOfNeighboring(workingTiles[0], tileId),
+                Vector2.Distance(spawnPoint, workingTiles[0]));
+
+            //determine best tile placement position from working tiles
+            for (int j = 1; j < workingTiles.Count; j++)
+            {
+                float value = valueFunc.Invoke(
+                (Vector2)(workingTiles[i] - spawnPoint),
+                NumberOfNeighboring(workingTiles[i], tileId),
+                Vector2.Distance(spawnPoint, workingTiles[i]));
+                if (value > bestValue) { bestValue = value; bestPos = workingTiles[i]; }
+            }
+            //place tile
+            SetTileId(bestPos, tileId);
+            AddValidTiles(NeighboringTiles(bestPos));
+        }
+    }
+
+    /// <summary>
+    /// Is 'pos' neighboring a tile with 'tileId' 
+    /// diagonals allowed
+    /// </summary>
+    private bool IsNeighboring(Vector2Int pos, int tileId)
+    {
+        for (int i = 0; i < OFFSETS.Length; i++)
+        {
+            if (GetTileId(pos + OFFSETS[i]) == tileId) return true;
+        }
+        return false;
+    }
+    /// <summary>
+    /// How many 'tileId' tiles are neighboring pos?
+    /// </summary>
+    private int NumberOfNeighboring(Vector2Int pos, int tileId)
+    {
+        int n = 0;
+        for (int i = 0; i < OFFSETS.Length; i++)
+        {
+            if (GetTileId(pos + OFFSETS[i]) == tileId) n++;
+        }
+        return n;
+    }
+
+    private Vector2Int[] NeighboringTiles(Vector2Int pos)
+    {
+        Vector2Int[] tiles = new Vector2Int[OFFSETS.Length];
+        for (int i = 0; i < OFFSETS.Length; i++)
+        {
+            tiles[i] = pos + OFFSETS[i];
+        }
+        return tiles;
     }
 }
